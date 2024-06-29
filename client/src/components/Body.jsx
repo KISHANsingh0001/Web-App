81c285358d2c9da7ab88f40856fa011c83f71ff1
sumith reddy <sumithreddy.6080@gmail.com>
2024-06-29 11:08:02 +0530

    solve img not found error

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -4,6 +4,7 @@ import HomeImg1 from "../assets/HomeImg1.png";
 import RServices from "./RServices";
 import AutismServices from "./AutismServices";
 import Banner from "./Banner";
+import { NotAlone } from "./NotAlone";
 function Body() {
   return (
     <div className="BodyOuter">
@@ -24,8 +25,10 @@ function Body() {
         </span>
       </div>
       <RServices />
+      <NotAlone />
       <AutismServices />
       <Banner />
+      
     </div>
   );
 }
