4b6171191c133c434b1612a64a6b0f1dc80d8c92
sumith reddy <sumithreddy.6080@gmail.com>
2024-06-29 14:39:16 +0530

    footer done

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -8,7 +8,7 @@ import NotAlone from "./NotAlone";
 import AssessmentOffer from "./AssesmentOffer";
 function Body() {
   return (
-    <div className="BodyOuter">
+    <div className="BodyOuter ">
       <div className="flex flex-col md:mt-20 justify-center items-center gap-4 ">
         <span className="md:text-5xl text-3xl font-bold text-purple-500">
           Being Neurodivergent is
