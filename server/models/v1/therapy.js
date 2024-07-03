43938bce77468fed2020c859efb14ac61e8496e8
Vinamra Vaswani <vinamravaswani@gmail.com>
2024-07-03 22:11:06 +0530

    added new functions and routes to Therapy

--- server/models/v1/therapy.js
+++ server/models/v1/therapy.js
@@ -1,22 +1,11 @@
 const mongoose = require('mongoose');
 
 const therapySessionSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        required: true
-    },
-    timeSlot: {
-        type: String,
-        required: true
-    },
-    typeOfTherapy: {
-        type: [String],
-        default: []
-    }
+    email: { type: String, required: true},
+    date: { type: Date, required: true},
+    timeSlot: {type: String,required: true},
+    typeOfTherapy: { type: [String], default: []},
+    isDone: { type: Boolean, default: false},
 });
 
 
@@ -24,10 +13,38 @@ const Therapy = mongoose.model('Therapy', therapySchema);
 
 
 const addTherapySession = async(email,date,timeSlot,typeOfTherapy)=>{
-    const therapy = new Therapy({email, date, timeSlot, typeOfTherapy});
+    const therapy = new Therapy({email, date, timeSlot, typeOfTherapy, isDone: false});
     await therapy.save();
     console.log("Therapy session added successfully");
 }
 
+const enumerateTherapySessions = async() => {
+    try{
+        const therapySessions = await Therapy.find();
+        return therapySessions;
+    }
+    catch(err){
+        console.log("Error fetching therapy sessions", err);
+    }
+}
 
-module.exports = {addTherapySession};
+const findTherapySession = async(email) => {
+    try{
+        const therapySession = await Therapy.findOne({ email: email});
+        return therapySession;
+    }catch(err){
+        console.log("Error fetching therapy session", err);
+    }
+}
+
+const updateTherapySession = async(email, date, timeSlot, typeOfTherapy, isDone) => {
+    try{
+        await Therapy.updateOne({ email: email}, { date, timeSlot, typeOfTherapy, isDone});
+        console.log("Therapy session updated successfully");
+
+    }catch(err){
+        console.log("Error updating therapy session", err);
+    }
+}
+
+module.exports = {updateTherapySession,findTherapySession, enumerateTherapySessions , addTherapySession};
--- server/routes/v1/therapy.js
+++ server/routes/v1/therapy.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 import verifyToken from '../../middleware/verifyToken';
 const addTherapySession = require('../../models/v1/therapy');
+import {updateTherapySession,findTherapySession, enumerateTherapySessions , addTherapySession} from '../../models/v1/therapy';
 
 router.get('/api/v1/therapy', (req, res) => {
     res.json(
@@ -11,12 +12,30 @@ router.get('/api/v1/therapy', (req, res) => {
 );
 
 
-router.post('/api/v1/therapy',verifyToken, async(req, res) => {
+router.post('/api/v1/addTherapy',verifyToken, async(req, res) => {
     const email = req.user.email;
     const {date, timeSlot, typeOfTherapy} = req.body;
     await addTherapySession.addTherapySession(email, date, timeSlot, typeOfTherapy);
     mailUser(email, date, timeSlot, typeOfTherapy);
     res.json({success:true, message:"Therapy session added successfully"});
+});
+
+router.post('/api/v1/therapy/update', verifyToken, async(req, res) => {
+    const email = req.user.email;
+    const {date, timeSlot, typeOfTherapy, isDone} = req.body;
+    await updateTherapySession.updateTherapySession(email, date, timeSlot, typeOfTherapy, isDone);
+    res.json({success:true, message:"Therapy session updated successfully"});
+});
+
+router.post('/api/v1/therapy/find', verifyToken, async(req, res) => {
+    const email = req.user.email;
+    const therapySession = await findTherapySession.findTherapySession(email);
+    res.json(therapySession);
+});
+
+router.post('/api/v1/therapy/enumerateTherapy', verifyToken, async(req, res) => {
+    const therapySessions = await enumerateTherapySessions();
+    res.json(therapySessions);
 });
 
 module.exports = router;
