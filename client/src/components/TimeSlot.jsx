acea51b0bf725439b00203257a7bb056b8b4429c
mraziz01 <mraziz8341@gmail.com>
2024-09-09 23:23:51 +0530

    changed booconsultation

--- client/src/components/TimeSlot.jsx
+++ client/src/components/TimeSlot.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 
 const TimeSlots = ({ setTimeSlot }) => {
@@ -10,18 +11,25 @@ const TimeSlots = ({ setTimeSlot }) => {
       <p className="h-[36px] w-[202px] text-2xl font-normal font-poppins">
         Choose time slot
       </p>
-      <div className="mt-5 ">
-        {["9:00am-10:00am", "10:00am-11:00am", "11:00am-12:00pm", "12:00pm-1:00pm", "1:00pm-2:00pm", "2:00pm-3:00pm"].map((slot) => (
-          <div className="form-control m-2" key={slot}>
-            <label className="label cursor-pointer justify-start p-0">
+      <div className="flex flex-wrap gap-6 mt-5">
+        {[
+          "9:00am-10:00am",
+          "10:00am-11:00am",
+          "11:00am-12:00pm",
+          "12:00pm-1:00pm",
+          "1:00pm-2:00pm",
+          "2:00pm-3:00pm",
+        ].map((slot) => (
+          <div key={slot} className="flex-1 min-w-[200px]">
+            <label className="flex items-center cursor-pointer p-5 bg-white border-2 border-gray-200 rounded-lg shadow-md transition-all duration-200 hover:bg-green-50 hover:border-green-500">
               <input
                 type="radio"
                 name="timeSlot"
                 value={slot}
                 onChange={handleChange}
-                className="radio checked:bg-[#CB6BE5] border-[#CB6BE5] checked:shadow-none h-6 w-6"
+                className="radio checked:bg-[#CB6BE5] border-[#CB6BE5] h-6 w-6"
               />
-              <span className="label-text mx-3 h-[24px] font-inter text-[#19181A] font-normal text-sm">
+              <span className="ml-3 font-inter text-[#19181A] font-normal text-sm">
                 {slot}
               </span>
             </label>
